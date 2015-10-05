e5c0a5db637b9227d86a9586167073c4955b5b65
EricMentele <diviningsage@gmail.com>
2015-10-04 23:40:44 -0700

    Initial Commit

---
+++ Free Film CampTests/Free_Film_CampTests.swift
@@ -0,0 +1,36 @@
+//
+//  Free_Film_CampTests.swift
+//  Free Film CampTests
+//
+//  Created by Eric Mentele on 10/4/15.
+//  Copyright © 2015 Eric Mentele. All rights reserved.
+//
+
+import XCTest
+@testable import Free_Film_Camp
+
+class Free_Film_CampTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
